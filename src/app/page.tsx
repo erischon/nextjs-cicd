0ccf25d37c4e3dc43931fc840cc26cf835d559e6
erischon <66017574+erischon@users.noreply.github.com>
2023-09-24 09:20:06 +0200

    crud on tasks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,41 @@
-import FormAddTodo from "@/components/FormAddTodo";
 import { TodoItem } from "@/components/TodoItem";
 
 import { getTodos } from "@/libs/getTodos";
 import prisma from "@/libs/prismadb";
 
-async function toggleTodo(id: string, isDone: boolean) {
+/**
+ * @description Toggle task, set isDone to true or false
+ * @version 1.0.0
+ * @param id  The id of the task
+ * @param isDone  The status of the task
+ */
+async function toggleTask(id: string, isDone: boolean) {
   "use server";
 
   await prisma.todo.update({ where: { id }, data: { isDone } });
+}
+
+/**
+ * @description Edit task title
+ * @version 1.0.0
+ * @param id  The id of the task
+ * @param title  The new title of the task
+ */
+async function editTask(id: string, title: string) {
+  "use server";
+
+  await prisma.todo.update({ where: { id }, data: { title } });
+}
+
+/**
+ * @description Delete task
+ * @version 1.0.0
+ * @param id  The id of the task
+ */
+async function deleteTask(id: string) {
+  "use server";
+
+  await prisma.todo.delete({ where: { id } });
 }
 
 /**
@@ -21,11 +49,24 @@ export default async function Home() {
 
   return (
     <main className="my-10">
-      <ul className="pl-4">
-        {todos.map((todo) => (
-          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
-        ))}
-      </ul>
+      <div className="w-full">
+        <div className="grid grid-cols-12 px-5 py-2 mb-4 bg-slate-700 rounded-sm">
+          <div className="col-span-10 font-semibold">Tasks</div>
+          <div className="col-span-2 text-center font-semibold">Actions</div>
+        </div>
+
+        <div className="">
+          {todos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              toggleTask={toggleTask}
+              editTask={editTask}
+              deleteTask={deleteTask}
+            />
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,32 +1,57 @@
 "use client";
 
+import { BiEdit, BiTrash } from "react-icons/bi";
+
 type TodoItemProps = {
   id: string;
   title: string;
   isDone: boolean;
-  toggleTodo: (id: string, complete: boolean) => void;
+  toggleTask: (id: string, complete: boolean) => void;
+  editTask: (id: string, title: string) => void;
+  deleteTask: (id: string) => void;
 };
 
 /**
  * @description TodoItem component
  * @version 1.0.0
  */
-export function TodoItem({ id, title, isDone, toggleTodo }: TodoItemProps) {
+export function TodoItem({
+  id,
+  title,
+  isDone,
+  toggleTask,
+  editTask,
+  deleteTask,
+}: TodoItemProps) {
   return (
-    <li className="flex gap-1 items-center">
-      <input
-        id={id}
-        type="checkbox"
-        className="cursor-pointer peer"
-        defaultChecked={isDone}
-        onChange={(e) => toggleTodo(id, e.target.checked)}
-      />
-      <label
-        htmlFor={id}
-        className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
-      >
-        {title}
-      </label>
-    </li>
+    <div className="grid grid-cols-12 px-5 py-1">
+      <div className="col-span-10 flex gap-2 items-center">
+        <input
+          id={id}
+          type="checkbox"
+          className="cursor-pointer peer"
+          defaultChecked={isDone}
+          onChange={(e) => toggleTask(id, e.target.checked)}
+        />
+
+        <label
+          htmlFor={id}
+          className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
+        >
+          {title}
+        </label>
+      </div>
+
+      <div className="col-span-2 flex gap-3 items-center justify-center">
+        <BiEdit
+          className="cursor-pointer text-slate-300 text-lg"
+          onClick={() => editTask(id, title)}
+        />
+        <BiTrash
+          className="cursor-pointer text-slate-300 text-lg"
+          onClick={() => deleteTask(id)}
+        />
+      </div>
+    </div>
   );
 }

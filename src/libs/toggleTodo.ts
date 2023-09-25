d10fbcfed80c23190f46f7ea3a4bd5e278bc527f
erischon <66017574+erischon@users.noreply.github.com>
2023-09-25 11:45:11 +0200

    Delete toggleTodo.ts

--- src/libs/toggleTodo.ts
+++
@@ -1,9 +0,0 @@
-import prisma from "@/libs/prismadb";
-
-/**
- * @description Toggle a todo's isDone status
- * @version 1.0.0
- */
-export default async function toggleTodo(id: string, isDone: boolean) {
-  await prisma.todo.update({ where: { id }, data: { isDone } });
-}
